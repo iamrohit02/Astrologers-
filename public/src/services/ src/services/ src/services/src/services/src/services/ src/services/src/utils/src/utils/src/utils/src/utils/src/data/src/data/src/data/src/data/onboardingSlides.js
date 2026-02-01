a4b32a4bcbfe47d1ceb7c6537763e2cb693df68e
Iamrohit <githubcoder343@gmail.com>
2026-02-01 11:58:30 +0530

    Create onboardingSlides.js

---
+++ public/src/services/ src/services/ src/services/src/services/src/services/ src/services/src/utils/src/utils/src/utils/src/utils/src/data/src/data/src/data/src/data/onboardingSlides.js
@@ -0,0 +1,23 @@
+export const ONBOARDING_SLIDES = [
+  {
+    id: 1,
+    image: '/images/onboarding/palm-scan.png', // We will use CSS/SVG placeholders
+    titleKey: 'onboarding.slide1_title',
+    descKey: 'onboarding.slide1_desc',
+    icon: 'palm'
+  },
+  {
+    id: 2,
+    image: '/images/onboarding/tarot.png',
+    titleKey: 'onboarding.slide2_title',
+    descKey: 'onboarding.slide2_desc',
+    icon: 'tarot'
+  },
+  {
+    id: 3,
+    image: '/images/onboarding/chat.png',
+    titleKey: 'onboarding.slide3_title',
+    descKey: 'onboarding.slide3_desc',
+    icon: 'chat'
+  }
+];
